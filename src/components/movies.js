dfd3a571a7b9d564870f4f61f4af8851a7c02370
cijbest <cijbest@gmail.com>
2020-03-18 19:43:51 +0900

    # Revision

--- src/components/movies.js
+++ src/components/movies.js
@@ -5,17 +5,17 @@ import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres}) {
     return (
-        <Link to={{
-                pathname: `/movie/${id}`,
-                state: {
-                    year,
-                    title,
-                    summary,
-                    poster,
-                    genres
-                }
-            }}>
             <div className="movie">
+                <Link to={{
+                    pathname: `/movie/${id}`,
+                    state: {
+                        year,
+                        title,
+                        summary,
+                        poster,
+                        genres
+                    }
+                }}>
                 <img src={poster} alt={title} title={title}/>
                 <div className="movie__date">
                     <h3 className="movie__title">{title}</h3>
@@ -28,8 +28,8 @@ function Movie({id, year, title, summary, poster, genres}) {
                     <p className="movie__summary">{summary.slice(0, 140)}...</p>
                     
                 </div>
+                </Link>
             </div>
-        </Link>
     )
 }
 
